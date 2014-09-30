8248237402ea6da3fec780ac939646b2f348c0a4
John Osborne <ozborn@uab.edu>
2014-09-30 13:43:32 -0500

    Eclipse specific settings to allow building in Eclipse. Additionally changed training directories so that it matches those downloaded for SemEval

--- ClearClinical/src/main/java/edu/colorado/clear/clinical/ner/pipeline/TrainTestPipeline.java
+++ ClearClinical/src/main/java/edu/colorado/clear/clinical/ner/pipeline/TrainTestPipeline.java
@@ -42,8 +42,8 @@ public class TrainTestPipeline
 
 	public static String resourceDirPath = "src/main/resources/";
 
-	public static String semeval_train = resourceDirPath + "semeval-2014-task-7/data/trainsubset";
-	public static String semeval_devel = resourceDirPath + "semeval-2014-task-7/data/develsubset";
+	public static String semeval_train = resourceDirPath + "semeval-2014-task-7/data/train";
+	public static String semeval_devel = resourceDirPath + "semeval-2014-task-7/data/devel";
 	public static String abbrFile = resourceDirPath + "data/abbr.txt";
 	public static String cuiMapFile = resourceDirPath + "data/cuiMap.txt";
 
